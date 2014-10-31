71d3a465d1ac25e4fb35ef9937a7033de75771b2
runninMike <mike.runyon@ymail.com>
2014-10-31 16:06:05 -0700

    Moving pipes
    Made a generator for the pipes and make them move instead of the map, or
    camera. I'm still not finished with it.

---
+++ Assets/Scripts/GenerateScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateScript : MonoBehaviour {
+
+	public GameObject rocks;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 1f);
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(rocks);
+	}
+}
---
+++ Assets/Scripts/PipeMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeMovement : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-4, 0);
+	
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody2D.velocity = velocity;
+	}
+}
