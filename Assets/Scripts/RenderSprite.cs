44f1e85cc896b620c4ecbfff2ec6fc01846fdd26
muffinman007 <anon@anon.com>
2014-10-30 23:14:24 -0700

    add 4 new scense, and placement sprite holder, also add Delta Quad bulletin board texture image

---
+++ Assets/Scripts/RenderSprite.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderSprite : MonoBehaviour {
+
+	public Sprite sprite;
+
+	SpriteRenderer spriteRenderer;
+
+	public float x;
+	public float y;
+	
+	// Use this for initialization
+	void Start (){
+		transform.position = new Vector3(x, y, -1.0f);
+		spriteRenderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		spriteRenderer.sprite = sprite;
+	}
+}
