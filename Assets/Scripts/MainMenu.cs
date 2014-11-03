be9e57c7e66f38090b5119c1b808a095967978ba
lanst <norlan_prudente@hotmail.com>
2014-11-03 03:08:45 -0800

    added play button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(Screen.width/4f,Screen.height/2.85f,Screen.width/8,Screen.height/15),"Play"))
+		    Application.LoadLevel("GamePlay");
+	}
+}
