b41c2a165614aaa646a6100edfb967b4015f172c
lanst <norlan_prudente@hotmail.com>
2014-11-03 19:31:15 -0800

    Main menu button and game over box

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	bool gameOver = true;
+
+	void OnGUI()
+	{
+		if(gameOver)
+			GUI.Box(new Rect(camera.pixelWidth/12, camera.pixelHeight/12,camera.pixelWidth - (camera.pixelWidth/10)*1.7f,camera.pixelHeight - (camera.pixelHeight/10)*1.7f), "Game Over");
+
+	}
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,5 +7,11 @@ public class MainMenu : MonoBehaviour {
 	{
 		if (GUI.Button(new Rect(Screen.width/4f,Screen.height/2.85f,Screen.width/8,Screen.height/15),"Play"))
 		    Application.LoadLevel("GamePlay");
+
+		if (GUI.Button(new Rect(Screen.width/4f,Screen.height/1.5f,Screen.width/8,Screen.height/15),"Quit"))
+			Application.Quit();
+
+		if (GUI.Button(new Rect(Screen.width/4f,(Screen.height/1.5f) + 50,Screen.width/8,Screen.height/15),"Credit"))
+			Application.LoadLevel("Credit");
 	}
 }
