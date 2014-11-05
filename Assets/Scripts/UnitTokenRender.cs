8dd4e8ead084a5b7997bf3beae0f094b978d9bd8
muffinman007 <anon@anon.com>
2014-11-04 21:29:00 -0800

    add unit token object for the scoring. add "shinny" animation to it

---
+++ Assets/Scripts/UnitTokenRender.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitTokenRender : MonoBehaviour{
+
+	public Sprite[] sprites;
+	SpriteRenderer spriteRenderer;
+
+	public int fps;
+	float timer = 0.0f;
+	public float timerLimit = 1.5f;		// time between each animation
+
+	public int playHowManyTime = 2;     // play the animation how many times
+	int playCounter = 0;
+	
+	// Use this for initialization
+	void Start (){
+		spriteRenderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		timer += Time.deltaTime;
+
+		if(timer >= timerLimit){
+			int index = ((int)(timer * fps)) % sprites.Length;
+			
+			if(index == 0)
+				++playCounter;
+			
+			if( !(playCounter > playHowManyTime) )
+				spriteRenderer.sprite = sprites[index];
+			else{
+				spriteRenderer.sprite = sprites[0];
+				playCounter = 0;
+				timer = 0.0f;
+			}
+		}
+		else
+			spriteRenderer.sprite = sprites[0];
+	}
+
+
+
+}
