111dcd2ee41cccfbe1b5c458f281bf6a88fcdd85
lanst <norlan_prudente@hotmail.com>
2014-11-06 02:34:11 -0800

    GameOver updated w/ background and message

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,13 +2,30 @@
 using System.Collections;
 
 public class GameOver : MonoBehaviour {
+	public GUISkin skin;
 
 	bool gameOver = true;
+	int score = 0;
 
 	void OnGUI()
 	{
+		GUI.skin = skin;
+
 		if(gameOver)
-			GUI.Box(new Rect(camera.pixelWidth/12, camera.pixelHeight/12,camera.pixelWidth - (camera.pixelWidth/10)*1.7f,camera.pixelHeight - (camera.pixelHeight/10)*1.7f), "Game Over");
+		{
+			GUI.Box(new Rect(camera.pixelWidth/12, camera.pixelHeight/12,
+			                 camera.pixelWidth - (camera.pixelWidth/10)*1.7f,
+			                 camera.pixelHeight - (camera.pixelHeight/10)*1.7f),
+			        		 "Game Over\nCongratulation!",
+			        		 skin.GetStyle("gameOver"));
+
+			GUI.Label(new Rect(camera.pixelWidth/10, camera.pixelHeight/2,
+			                   camera.pixelWidth - (camera.pixelWidth/10)*1.7f,
+			                   camera.pixelHeight - (camera.pixelHeight/10)*1.7f),
+					           "Here is your diploma from the CBT college.\nwith the total unit of\n" +
+			          		   score.ToString() + " unit(s)",
+			          		   skin.GetStyle("message"));
+		}
 
 	}
 }
