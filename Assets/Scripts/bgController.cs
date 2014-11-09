5bf60e96379aa79ff28e651231c25526dc1178c0
muffinman007 <anon@anon.com>
2014-11-08 18:18:07 -0800

    create sky background prefab. update skybg contoller to instantaite a new sky bg and destory itself after x movement

--- Assets/Scripts/bgController.cs
+++ Assets/Scripts/bgController.cs
@@ -8,7 +8,15 @@ public class bgController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update (){
+		// got the magic number by manually moving the object around the editor until i get the desired coordinates
+		// magic numbers are multiple of .03
+		// creating a new bg object
+		if(transform.position.x <= -6.36f)
+			Instantiate(Resources.Load<GameObject>("SkyBg"), new Vector3(6.42f, 0.0f, 0.0f), Quaternion.identity);
+ 
+		// destory self
+		if(transform.position.x <= -12.84)
+			Destroy(gameObject);
 	}
 }
